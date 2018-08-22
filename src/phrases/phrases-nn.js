b73ef4803e7a6bfacd47b70e90fae1102e5e2f1b
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro-2.local>
2018-08-22 10:02:17 +0200

    Phrases updated

--- src/phrases/phrases-nn.js
+++ src/phrases/phrases-nn.js
@@ -6,171 +6,338 @@
  *
  */
 
+const titleTemplate = ' - NDLA';
+
 const phrases = {
+  htmlTitles: {
+    titleTemplate,
+    welcomePage: `Framsida${titleTemplate}`,
+    topicPage: 'Emne',
+    subjectsPage: `Velg fag${titleTemplate}`,
+    searchPage: `Søk${titleTemplate}`,
+    notFound: `Sida finst ikkje${titleTemplate}`,
+  },
+  newsLetter: {
+    heading: 'Nyheitsbrev',
+    description: 'Hald deg oppdatert. Abonnér på siste nytt frå NDLA.',
+    mainLinkName: 'Meld deg på',
+    iconLinkName: 'Meld deg på nyheitsbrev',
+  },
+  askNDLA: 'Spør NDLA',
   articlePage: {
-    errorDescription:
-      'Vi beklager, men en feil oppsto under lasting av ressursen.',
+    errorDescription: 'Orsak, ein feil oppstod under lasting av ressursen.',
     error404Description:
-      'Vi beklager, men vi kunne ikke finne ressursen du leter etter.',
+      'Orsak, vi kunne ikkje finne ressursen du leiter etter.',
   },
   notFoundPage: {
-    errorDescription:
-      'Vi beklager, men vi fant ikke siden du prøvde å komme til.',
+    errorDescription: 'Orsak, vi fann ikkje sida du prøvde å kome til.',
   },
   searchPage: {
-    noHits: 'Ingen artikler samsvarte med søket ditt på: {query}',
+    noHits: 'Ingen artiklar samsvarte med søket ditt på: {query}',
+    search: 'Søk',
+    abilities: 'Egenskaper',
+    close: 'Lukk',
+    searchFieldPlaceholder:
+      'Søk i fagstoff, oppgåver og aktivitetar eller læringsstiar',
+    label: {
+      contentTypes: 'Innhaldstypar',
+      levels: 'Nivå',
+      'language-filter': 'Språk',
+      subjects: 'Fag',
+    },
+    showLabel: {
+      contentTypes: 'Fleire innhaldstypar',
+      levels: 'Fleire nivå',
+      'language-filter': 'Fleire språk',
+      subjects: 'Bytt fag',
+    },
+    hideLabel: {
+      contentTypes: 'Færre innhaldstypar',
+      levels: 'Færre nivå',
+      'language-filter': 'Færre språk',
+      subjects: 'Færre fag',
+    },
+    searchField: {
+      contentTypeResultShowMoreLabel: 'Sjå fleire resultat',
+      contentTypeResultShowLessLabel: 'Sjå færre resultat',
+      allResultButtonText: 'Vis alle søketreff',
+      searchResultHeading: 'Forslag:',
+      contentTypeResultNoHit: 'Ingen treff',
+    },
+    searchResultMessages: {
+      searchStringLabel: 'Du søkte på:',
+      subHeading: '{totalCount} treff i Ndla',
+    },
+    searchResultListMessages: {
+      subjectsLabel: 'Opne i fag:',
+      noResultHeading: 'Hmm, ikkje noko innhald ...',
+      noResultDescription:
+        'Vi har dessverre ikkje noko å tilby her. Om du vil foreslå noko innhald til dette området, kan du bruke Spør NDLA som du finn nede til høgre på skjermen.',
+    },
+    searchPageMessages: {
+      filterHeading: 'Filter',
+      resultHeading: '{totalCount} treff i Ndla',
+      narrowScreenFilterHeading: '{totalCount} treff på «{query}»',
+      dropdownBtnLabel: 'Flerie innhaldstypar',
+    },
+    searchFilterMessages: {
+      backButton: 'Tilbake til filter',
+      filterLabel: 'Vel fag',
+      confirmButton: 'Bruk fag',
+      hasValuesButtonText: 'Byt fag',
+      noValuesButtonText: 'Vel fag',
+    },
   },
   subjectPage: {
-    errorDescription:
-      'Vi beklager, men en feil oppsto under lasting av emnene.',
+    errorDescription: 'Orsak, ein feil oppstod under lasting av emnene.',
     tabs: {
-      topics: 'Emner',
+      topics: 'Emne',
+    },
+    subjectShortcuts: {
+      heading: 'Gå direkte til',
+      showMore: 'Vis fleire',
+      showLess: 'Vis færre',
+    },
+    mostRead: {
+      heading: 'Mest lese',
+    },
+    editorsChoices: {
+      heading: 'Litt forskjellig frå faget',
+      unknown: 'Ukjend',
+    },
+    subjectArchive: {
+      heading: 'Aktuelt',
+      archive: 'Arkiv',
+      close: 'Lukk',
+    },
+    subjectFilter: {
+      label: 'Filter',
+    },
+    newContent: {
+      heading: 'Nytt innhald',
     },
   },
   subjectsPage: {
-    errorDescription:
-      'Vi beklager, men en feil oppsto under lasting av fagene.',
-    chooseSubject: 'Velg fag',
+    errorDescription: 'Orsak, ein feil oppstod under lasting av faga.',
+    chooseSubject: 'Vel fag',
   },
   topicPage: {
-    topicErrorDescription:
-      'Vi beklager, men en feil oppsto under lasting av emne innholdet.',
     articleErrorDescription:
-      'Vi beklager, men en feil oppsto under lasting av emne beskrivelsen.',
+      'Orsak, ein feil oppstod under lasting av emneskildringa.',
     topic: 'EMNE',
     topics: 'Emner',
   },
   welcomePage: {
-    subjects: 'Fag',
-    search: 'Gå til søk',
-    errorDescription:
-      'Vi beklager, men en feil oppsto under lasting av fagene.',
+    search: 'Søk',
+    highlighted: 'Aktuelt',
+    heading: {
+      heading: 'Nasjonal digital læringsarena',
+      searchFieldPlaceholder: 'Søk etter f.eks emne, lærestoff, nykelord...',
+      messages: {
+        searchFieldTitle: 'Søk',
+        menuButton: 'Meny',
+      },
+      links: {
+        aboutNDLA: 'Om NDLA',
+        changeLanguage: 'Skift språk',
+      },
+    },
+    socialMedia: {
+      heading: 'Følg oss',
+      description:
+        'NDLA har mange Facebook- og Twitterkontoar. Finn den som passar for deg, og følg oss!',
+      mainLink: {
+        name: 'Følg oss',
+      },
+    },
+    aboutNDLA: {
+      heading: 'Om NDLA',
+      description:
+        'NDLAs visjon er å lage gode, opne digitale læremiddel for alle fag i videregående opplæring og støtte opp om elevar og lærarar i aktivt og deltakende læringsarbeid.',
+      mainLink: {
+        name: 'Meir om Ndla',
+      },
+    },
+    category: {
+      fellesfag: 'Fellesfag',
+      yrkesfag: 'Yrkesfag',
+      studiespesialiserende: 'Studiespesialiserende',
+      imported: 'Spoltefag',
+    },
+    errorDescription: 'Orsak, ein feil oppstod under lasting av faga.',
+    betaMessages: {
+      heading: 'Hei!',
+      text: `Du har nå kommet inn på de nye nettsidene våre.
+        Vi prøver ut en ny løsning i utvalgte fag og vil
+        forsikre oss om at alt virker som det skal før skolestart.
+        Får du problemer på sidene, setter vi pris på om du sender
+        oss en melding. Kontakt oss på "Spør NDLA" nederst til
+        venstre på sidene.`,
+      readmoreText: 'Les mer på om.ndla.no',
+      readmoreLink: 'http://www.om.ndla.no',
+      buttonText: 'Ok, nå vet jeg det',
+    },
   },
   meta: {
     description:
-      'Kvalitetssikrede fritt tilgjengelige nettbaserte læremidler for videregående opplæring',
+      'Kvalitetssikra fritt tilgjengelege nettbaserte læremiddel for videregåande opplæring',
   },
   masthead: {
     menu: {
       close: 'Lukk',
       goTo: 'Gå til',
       search: 'Søk',
-      subjectOverview: 'Fagoversikt',
+      subjectOverview: 'Alle fag',
       title: 'Meny',
+      subjectPage: 'Fagforside',
+      learningResourcesHeading: 'Læringsressurser',
+      back: 'Tilbake',
+      contentTypeResultsShowMore: 'Vis mer',
+      contentTypeResultsShowLess: 'Vis mindre',
+      contentTypeResultsNoHit: 'Ingen treff',
     },
   },
   logo: {
     altText: 'Nasjonal digital læringsarena',
   },
   resource: {
-    errorDescription:
-      'Vi beklager, men en feil oppsto under lasting av emne ressurser.',
-    noCoreResourcesAvailable: 'Det er ikke noe kjernestoff tilgjengelig.',
-    activateAdditionalResources: 'Vis tilleggsstoff',
-    toggleFilterLabel: 'Tilleggsstoff',
+    errorDescription: 'Orsak, ein feil oppstod under lasting av emneressursar.',
+    noCoreResourcesAvailableUnspecific:
+      'Det er ikkje noko kjernestoff tilgjengeleg.',
+    noCoreResourcesAvailable:
+      'Det er ikkje noko kjernestoff tilgjengeleg for {name}.',
+    toggleFilterLabel: 'Tilleggsressursar',
+    activateAdditionalResources: 'Vis tilleggsressursar',
+    label: 'Læringsressursar',
+    shortcutButtonText: 'Lærestoff',
+    tooltipCoreTopic: 'Kjernestoff er fagstoff',
+    tooltipAdditionalTopic: 'Tilleggsstoff er fagstoff',
+    additionalTooltip: 'Tilleggsstoff',
+    dialogTooltip: 'Kva er kjernestoff og tilleggsstoff?',
+    dialogHeading: 'Kjernestoff og tilleggsstoff',
+    dialogText1:
+      'Når du lærar deg kjernestoffet skaffar du deg den kompetansen som beskrives i læreplanen for faget.',
+    dialogText2:
+      'Tilleggstoff er innhald i faget som du kan velje i tillegg til kjernestoffet. Gjennom tilleggsstoffet kan du fordjupe deg i et emne eller tilnærma deg emnet på en anna måte.',
     showLess: 'Vis mindre',
     showMore: 'Vis mer',
   },
-  searchForm: {
-    placeholder: 'Søk etter artikler',
-    btn: 'Søk',
-    order: {
-      relevance: 'Relevans',
-      title: 'Alfabetisk',
-    },
-  },
   article: {
     lastUpdated: 'Sist oppdatert',
-    edition: 'Utgave',
-    publisher: 'Utgiver',
-    closeLicenseBox: 'Lukk boks',
-    openLicenseBox: 'Bruk artikkel',
+    edition: 'Utgåve',
+    publisher: 'Utgjevar',
+    closeLabel: 'Lukk',
+    useContent: 'Bruk innhald',
+    additionalLabel: 'Tilleggsstoff',
   },
   subject: {
     associatedTopics: 'Tilhørende emner',
   },
   license: {
-    heading: 'Slik gjenbruker du innhold',
-    learnMore: 'Lær mer om åpne lisenser',
+    heading: 'Slik gjenbruker du innhald',
+    learnMore: 'Lær meir om opne lisensar',
     copyTitle: 'Kopier referanse',
     hasCopiedTitle: 'Kopiert!',
     embed: 'Bygg inn',
-    embedCopied: 'Kopierte bygg inn kode!',
+    embedCopied: 'Kopierte innbyggingskode!',
     download: 'Last ned',
     tabs: {
       text: 'Tekst',
-      images: 'Bilder',
+      images: 'Bilde',
       audio: 'Lyd',
       video: 'Video',
     },
     images: {
-      heading: 'Slik bruker du bilder fra artikkelen',
+      heading: 'Slik bruker du bilder frå artikkelen',
       description:
-        'Husk å kopier teksten som skal legges ved bildet der du bruker det.',
+        'Hugs å kopiera teksten som skal leggjast ved bildet der du bruker det.',
       rules: 'Regler for bruk av bildet:',
-      source: 'Kilde',
+      source: 'Kjelde',
       title: 'Tittel',
     },
     text: {
-      heading: 'Slik bruker du tekst fra artikkelen',
+      heading: 'Slik bruker du tekst frå artikkelen',
       description:
-        'Artikkelen kan være satt sammen av flere ulike tekster, som listes opp her.',
-      rules: 'Regler for bruk av teksten:',
+        'Artikkelen kan vera samansett av fleire ulike tekstar som vert lista her.',
+      rules: 'Reglar for bruk av teksten:',
       published: 'Publiseringsdato',
     },
     audio: {
       heading: 'Slik bruker du lydfiler',
       description:
-        'Husk å kopier teksten som skal legges ved lydfilen der du bruker den.',
-      rules: 'Regler for bruk av lydfilen:',
+        'Hugs å kopiera teksten som skal leggjast ved lydfila der du bruker ho.',
+      rules: 'Regler for bruk av lydfila:',
     },
     video: {
       heading: 'Slik bruker du video fra artikkelen',
       description:
-        'Husk å kopier teksten som skal legges ved videoen der du bruker den.',
+        'Hugs å kopiera teksten som skal leggjast ved videoen der du bruker han.',
       rules: 'Regler for bruk av videoen:',
     },
     creditType: {
       originator: 'Opphavsmann',
+      authorDesc: 'Denne artikkelen er laget av flere opphavsmenn',
       photographer: 'Fotograf',
-      artist: 'Kunstner',
+      artist: 'Kunstnar',
       editorial: 'Redaksjonelt',
-      writer: 'Forfatter',
-      scriptwriter: 'Manusforfatter',
-      reader: 'Innleser',
-      translator: 'Oversetter',
+      writer: 'Forfattar',
+      scriptwriter: 'Manusforfattar',
+      reader: 'Innlesar',
+      translator: 'Omsetjar',
       director: 'Regissør',
       illustrator: 'Illustratør',
       cowriter: 'Medforfatter',
       composer: 'Komponist',
-      processor: 'Bearbeider',
-      facilitator: 'Tilrettelegger',
-      linguistic: 'Språklig',
+      processor: 'Tilarbeidar',
+      facilitator: 'Tilretteleggjar',
+      linguistic: 'Språkleg',
       idea: 'Idé',
-      compiler: 'Sammenstiller',
+      compiler: 'Sammenstillar',
       correction: 'Korrektur',
-      rightsholder: 'Rettighetshaver',
+      rightsholder: 'Rettshaver',
       publisher: 'Forlag',
       distributor: 'Distributør',
       supplier: 'Leverandør',
     },
   },
-  breadcrumb: {
-    subjectsLinkText: 'Fag',
-    label: 'Du er her:',
-  },
   errorMessage: {
-    title: 'Oops, noe gikk galt',
-    description: 'Vi beklager, men en feil oppsto.',
+    title: 'Ops, noko gjekk gale',
+    description: 'Orsak, ein feil oppstod.',
+    linksTitle: 'Kjem igang:',
     back: 'Tilbake',
-    goToFrontPage: 'Gå til forsiden',
+    goToFrontPage: 'Gå til forsida',
   },
   footer: {
     aboutNDLA: 'Om NDLA',
-    selectLanguage: 'Velg språk (language): ',
-    footerInfo: 'Nettstedet er utarbeidet av NDLA med åpen kildekode.',
-    footerEditiorInChief: 'Ansvarlig redaktør: ',
-    footerManagingEditor: 'Utgaveansvarlig: ',
+    selectLanguage: 'Vel språk (language): ',
+    footerInfo: 'Nettstaden er utarbeida av NDLA med open kjeldekode.',
+    footerEditiorInChief: 'Ansvarleg redaktør: ',
+    footerManagingEditor: 'Utgåveansvarleg: ',
+  },
+  contentTypes: {
+    all: 'Alle',
+    subject: 'Emne',
+    'topic-article': 'Emne',
+    'learning-path': 'Læringssti',
+    'subject-material': 'Fagstoff',
+    'tasks-and-activities': 'Oppgåver og aktivitetar',
+    'external-learning-resources': 'Ekstern læringsressurs',
+    'source-material': 'Kjeldemateriale',
+    'assessment-resources': 'Vurderingsressurs',
+  },
+  languages: {
+    nb: 'Bokmål',
+    nn: 'Nynorsk',
+    en: 'Engelsk',
+    fr: 'Fransk',
+    de: 'Tysk',
+    se: 'Samisk',
+    es: 'Spansk',
+    zh: 'Kinesisk',
+    unkown: 'Ukjent',
+  },
+  breadcrumb: {
+    toFrontpage: 'Til framsida',
   },
 };
 
